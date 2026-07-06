aed45d709638e3b1c6df6b16ba6090c3cf22ed75
Kamilla Macêdo <macedo.kamilla@outlook.com>
2026-07-06 16:05:11 -0300

    feat: complete reset timer logic and toggle button visibility

--- script.js
+++ script.js
@@ -1,68 +1,83 @@
-const html = document.querySelector('html');
-const focusbtt = document.querySelector('.app__card-button--focus');
-const shortbtt = document.querySelector('.app__card-button--short');
-const longbtt = document.querySelector('.app__card-button--long');
-const startbtt = document.querySelector('.app__card-primary-button')
-const timerDisplay = document.querySelector('.app__card-timer');
-const resetbtt = document.querySelector('.app__card-reset-button');
+const html = document.querySelector("html");
+const focusbtt = document.querySelector(".app__card-button--focus");
+const shortbtt = document.querySelector(".app__card-button--short");
+const longbtt = document.querySelector(".app__card-button--long");
+const startbtt = document.querySelector(".app__card-primary-button");
+const timerDisplay = document.querySelector(".app__card-timer");
+const resetbtt = document.querySelector(".app__card-reset-button");
 
 let timeInSeconds = 1500;
 let isRunning = false;
 let intervalId = null;
 
-
-focusbtt.addEventListener('click', () =>{
-    html.setAttribute('data-context', 'focus');
-    timeInSeconds = 1500;
-    showTimer()
+focusbtt.addEventListener("click", () => {
+  html.setAttribute("data-context", "focus");
+  timeInSeconds = 1500;
+  showTimer();
 });
 
-shortbtt.addEventListener('click', () =>{
-    html.setAttribute('data-context', 'short-break');
-    timeInSeconds = 300;
-    showTimer()
+shortbtt.addEventListener("click", () => {
+  html.setAttribute("data-context", "short-break");
+  timeInSeconds = 300;
+  showTimer();
 });
 
-longbtt.addEventListener('click', () =>{
-    html.setAttribute('data-context', 'long-break');
-    timeInSeconds = 900;
-    showTimer()
+longbtt.addEventListener("click", () => {
+  html.setAttribute("data-context", "long-break");
+  timeInSeconds = 900;
+  showTimer();
 });
 
-startbtt.addEventListener('click', toggleTimer);
+startbtt.addEventListener("click", toggleTimer);
+
+resetbtt.addEventListener("click", resetTimer);
 
 function toggleTimer() {
-    if (isRunning === true) {
-        clearInterval(intervalId);
-        intervalId = null; 
-        isRunning = false;
-        resetbtt.classList.add('hidden');
-    }else{
-        intervalId = setInterval(startTimer, 1000);
-        isRunning = true;
-        resetbtt.classList.remove('hidden'); 
-    }
-    
+  if (isRunning === true) {
+    clearInterval(intervalId);
+    intervalId = null;
+    isRunning = false;
+    resetbtt.classList.add("hidden");
+  } else {
+    intervalId = setInterval(startTimer, 1000);
+    isRunning = true;
+    resetbtt.classList.remove("hidden");
+  }
 }
 
 function startTimer() {
-    timeInSeconds -= 1;
-    showTimer();
-    
-    if (timeInSeconds === 0) {
-        clearInterval(intervalId);
-        intervalId = null; 
-        isRunning = false;
-    }
+  timeInSeconds -= 1;
+  showTimer();
 
+  if (timeInSeconds === 0) {
+    clearInterval(intervalId);
+    intervalId = null;
+    isRunning = false;
+  }
 }
 
-function showTimer(){
-    const minutos = Math.floor(timeInSeconds / 60);
-    const segundos = timeInSeconds % 60;
-    const minutosFormatados = String(minutos).padStart(2,'0');
-    const segundosFormatados = String(segundos).padStart(2,'0');
-    timerDisplay.innerText = `${minutosFormatados}:${segundosFormatados}`
+function showTimer() {
+  const minutos = Math.floor(timeInSeconds / 60);
+  const segundos = timeInSeconds % 60;
+  const minutosFormatados = String(minutos).padStart(2, "0");
+  const segundosFormatados = String(segundos).padStart(2, "0");
+  timerDisplay.innerText = `${minutosFormatados}:${segundosFormatados}`;
 }
 
 showTimer();
+
+function resetTimer() {
+  clearInterval(intervalId);
+  intervalId = null;
+  isRunning = false;
+  if (html.getAttribute("data-context") === "focus") {
+    timeInSeconds = 1500;
+  } else if (html.getAttribute("data-context") === "short-break") {
+    timeInSeconds = 300;
+  } else if (html.getAttribute("data-context") === "long-break"){
+    timeInSeconds = 900;
+  }
+
+  showTimer()
+  resetbtt.classList.add("hidden");
+}
